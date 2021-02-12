a9ab0852bf790f919b6865fa9db4da832aa28201
chenao1997 <chenao@coruniamericana.edu.co>
2021-02-12 17:28:37 -0500

    Update bdtiendas1.sql

--- bdtiendas1.sql
+++ bdtiendas1.sql
@@ -1,1 +1,12 @@
 
+CREATE TABLE `cliente` (
+  `idCliente` int NOT NULL AUTO_INCREMENT,
+  `nombres` varchar(45) DEFAULT NULL,
+  `apellidos` varchar(45) DEFAULT NULL,
+  `telefono` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `ciudad` varchar(45) DEFAULT NULL,
+  `pais` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idCliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+© 2021 GitHub, Inc.
